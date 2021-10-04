e413e2691c7cb1ab6fff66ab47b53a367f7041b1
Marco Miduri <mmiduri.job@gmail.com>
2021-10-04 22:48:18 +0200

    add menu ainmation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     // console.log(props)
     return (
         <div className={`logo ${props.isLandingVisible ? '' : 'visible'}`}>
-            <h1><Link to='/'>Sami</Link></h1>
+            <h1><a href='/' onClick={(e) => {e.preventDefault(); setTimeout(() => {window.location.href = '/'}, 1000)}}>Sami</a></h1>
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+
 const Navigation = () => {
+
+    const [className, setClassName] = useState('')
+
+    const elements = [
+        'about',
+        'contacts'
+    ]
+
+    useEffect(() => {
+        setClassName('shown')
+        return () => console.log('unmount')
+    }, [])
+
     return (
         <nav className='lateral'>
             <ul>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contacts'>Contacts</Link></li>
+                {
+                    elements.map((elem, index) => {
+                        return <li key={index} className={elements[index]  + ' ' + className}><Link to={'/' + elem}>{elem.charAt(0).toUpperCase() + elem.slice(1)}</Link></li>
+                    })
+                }
             </ul>
         </nav>
     )
