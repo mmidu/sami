08ec31c959424d5f8356698a1ba2abc86f789249
luislopez <lewis.mlopez@gmail.com>
2021-10-06 10:16:54 +0200

    detached head

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -11,8 +11,15 @@ class Contacts extends Component {
                     <ul>
                         <li>Email:<a href="mailto:samischinaia@gmail.com">samischinaia@gmail.com</a></li>
                         <li>IG: <a  target="_blank" href="https://www.instagram.com/alwaysthesam/" rel="noreferrer">Alwaysthesam</a></li>
-                
                     </ul>
+                    <span>Partner and creative director</span>
+                        <ul>
+                            <li><a href="https://hipcool.studio/" target="_blank">@ hipcool.studio</a></li>
+                        </ul>    
+                    <span>Films i like</span>
+                        <ul> 
+                            <li><a href="https://www.instagram.com/bestindependentfilms/" target="_blank">@ bestindependentfilms</a></li>
+                        </ul>    
                 </div>
             </TextLayout>
         )
